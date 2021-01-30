9c8024640d6872c37238f2b8178352874582d515
Pedro Henrique <ph.camargo23@gmail.com>
2021-01-30 15:45:38 -0300

    Realocou scripts de Wordpress no projeto de templates

--- win/Wordpress/install/wp.bat
+++
@@ -1,2 +0,0 @@
-@ECHO OFF
-php "c:/tools/wp-cli/wp-cli.phar" %*
--- win/Wordpress/wp-remove.ps1
+++
@@ -1,8 +0,0 @@
-$origem = pwd
-$dir = '`loja-finta`';
-cd C:\xampp\htdocs;
-rm -Recurse -Force -Verbose .\$dir;
-mysql -u root -e "DROP DATABASE IF EXISTS $dir";
-cd $origem
-
-pause
